21cf562bded1eba51bb2e2005895011c18072c45
Dave <info@howmuchcomputer.com>
2014-08-10 21:07:09 -0400

    removed unused controller action

--- app/controllers/live_assets_controller.rb
+++ app/controllers/live_assets_controller.rb
@@ -1,15 +1,6 @@
 class LiveAssetsController < ApplicationController
   include ActionController::Live
 
-  def ping 
-    while true
-      response.stream.write DateTime.now.to_s + "\n"
-      sleep 1
-    end
-  rescue IOError
-    response.stream.close
-  end
-
   def sse
    begin
     response.headers["Cache-Control"] = "no-cache"
@@ -18,9 +9,9 @@ class LiveAssetsController < ApplicationController
     sse = LiveAssets::SSESubscriber.new
     sse.each { |msg| response.stream.write msg }
 
-  rescue IOError
-    sse.close
-    response.stream.close
-  end
+    rescue IOError
+      sse.close
+      response.stream.close
+    end
   end
 end
